bb3b7a34327a42f2f8d9f32f70922b490df9e1a7
Tony Aleynikov <tonyaleynikov@gmail.com>
2022-03-31 16:11:15 +0300

    Выполненые задания с 7 дня(c p-16 до  p-20)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'welcome#index'
+  end
+  root 'orders#calc'
+  #get 'orders/calc'
+  #get 'calc', to: 'orders#calc'
+    get 'hello/index'
+  
+
+  resources :orders do
+    member do
+      get 'approve'
+    end
+    get 'first', on: :collection
+  end
+
+  resources :users
+  #resources :orders строка 8 (если там есть ордерс, то эта строка не нужна?)
+
+  resource :login, only: [:show, :create, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
